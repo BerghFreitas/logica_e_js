e326478f8e282266c0c11bdb5165e19d463c34bb
BerghFreitas <berg2843@gmail.com>
2024-01-25 07:12:28 -0300

    chapter 3 and 4
    foi adicionado capitulo 3 e 4 e o modulo node na pasta livro js

---
+++ livro js/capitulo 4/ex3_1.js
@@ -0,0 +1,5 @@
+const prompt = required("promp-sync")()
+const num1 = Number(prompt("1 numero:"))
+const num2 = Number(prompt("2 numero: "))
+const soma =  num1 + num2
+console.log(`soma é : ${soma}`)
