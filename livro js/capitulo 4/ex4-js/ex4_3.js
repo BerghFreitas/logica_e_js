df753b01ba24f5efbd4561527cbafebf11dee2d3
BerghFreitas <berg2843@gmail.com>
2024-02-18 21:57:25 -0300

    operador ternario
    substitui um if else por operador condicional, alem de intender como funciona

--- livro js/capitulo 4/ex4-js/ex4_3.js
+++ livro js/capitulo 4/ex4-js/ex4_3.js
@@ -5,20 +5,26 @@ frm.addEventListener ('submit', (e) => {
     e.preventDefault()
     const nome = frm.inNome.value
     const masculino = frm.inMasc.checked
-
     const altura = Number(frm.inAltura.value)
-    const pesoH = 22
+    /*const pesoH = 22
     const pesoM = 21
-    let peso 
+    let peso */
     
-    if (masculino) {
-        peso = pesoH * Math.pow (altura, 2)
+    /*if (masculino) {
+        const peso = 22 * Math.pow (altura, 2)
+        resp.innerText = `${nome}: Seu peso ideal é  ${peso.toFixed(3)} Kg`
     } else {
-        peso = pesoM * Math.pow (altura, 2)
-    }    
-    resp.innerText = `${nome}: Seu peso ideal é  ${peso.toFixed(3)} Kg`
-}    
-
-
-
+        const peso = 21 * Math.pow (altura, 2)
+        resp.innerText = `${nome}: Seu peso ideal é  ${peso.toFixed(3)} Kg` 
+    }    //if e else que foi substituido
+    frm.addEventListener ('reset', () => {
+        resp.innerText = ""
+    }) //faz o evendo de reset agir
+})*/ 
+const peso = masculino ? 22 * Math.pow (altura,2) : 21 * Math.pow (altura , 2)
+resp.innerText = `seu peso ideal é ${peso}`
+{
+    frm.addEventListener ('reset', () => {
+        resp.innerText = ""
 })
+}})
---
+++ livro js/capitulo 4/ex4-js/ternario.js
@@ -0,0 +1,4 @@
+idade = 18
+const categoria = idade >= 18 ? "adulto" : "infantil" // operador ternario na pratica
+
+console.log(`${categoria}`)
